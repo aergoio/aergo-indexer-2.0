5b150449adc78da74ffdaeed3bc498ee17b16d56
Kjun Seo <kjseo@blocko.io>
2022-06-03 14:40:24 +0900

    change prefix alpha

--- check_alpha.sh
+++ check_alpha.sh
@@ -3,5 +3,5 @@ docker rm -f check_alpha
 docker run -it --name check_alpha --net=host --privileged \
 	-v $(pwd):/home \
 	-e AERGO_URL="alpha-api.aergo.io:7845" \
-	-e CHAIN_PREFIX="chain_" \
+	-e CHAIN_PREFIX="alpha_" \
 	ubuntu:21.10 bash /home/check_index_single.sh
--- sync_alpha.sh
+++ sync_alpha.sh
@@ -3,5 +3,5 @@ docker rm -f idx_alpha
 docker run -d -it --name idx_alpha --net=host --privileged \
 	-v $(pwd):/home \
 	-e AERGO_URL="alpha-api.aergo.io:7845" \
-	-e CHAIN_PREFIX="chain_" \
+	-e CHAIN_PREFIX="alpha_" \
 	ubuntu:21.10 bash /home/sync_index_single.sh
