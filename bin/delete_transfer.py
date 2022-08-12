40459f150086a6bc4e384138cdd45fbabc869fa3
Kjun Seo <kjseo@blocko.io>
2022-08-12 18:58:30 +0900

    .

--- bin/delete_transfer.py
+++ bin/delete_transfer.py
@@ -6,29 +6,29 @@ es = Elasticsearch('localhost:9200')
 
 prefix = sys.argv[1]
 
-s = Search(using=es, index=prefix+'_token')
+s = Search(using=es, index=prefix+'token')
 s.execute()
 token_list = [h.meta.id for h in s.scan()]
 
 
-print("delete _tokens_transfer")
-s = Search(using=es, index=prefix+'_token_transfer')
+print("delete tokens_transfer")
+s = Search(using=es, index=prefix+'token_transfer')
 s.aggs.bucket('tx', 'terms', field='address', size=10000)
 tx = s.execute()
 
 for x in tx.aggregations.tx.buckets:
     if x.key not in token_list :
         print("delete token tx", x.doc_count, x.key)
-        es.delete_by_query(index=prefix+'_token_transfer', body={"query":{"match": {"address": x.key}}})
+        es.delete_by_query(index=prefix+'token_transfer', body={"query":{"match": {"address": x.key}}})
 
 
-print("delete _account_tokens")
-s = Search(using=es, index=prefix+'_account_tokens')
+print("delete account_tokens")
+s = Search(using=es, index=prefix+'account_tokens')
 s.aggs.bucket('tx', 'terms', field='address', size=10000)
 tx = s.execute()
 
 for x in tx.aggregations.tx.buckets:
     if x.key not in token_list :
         print("delete account token", x.doc_count, x.key)
-        es.delete_by_query(index=prefix+'_account_tokens', body={"query":{"match": {"address": x.key}}})
+        es.delete_by_query(index=prefix+'account_tokens', body={"query":{"match": {"address": x.key}}})
 
