11a49e2cd63f1c2396d07e3c0ed5bb6fec071f05
Kjun Seo <kjseo@blocko.io>
2022-08-12 12:04:16 +0900

    fix main.sh

--- el_main_01.sh
+++ el_main_01.sh
@@ -9,12 +9,12 @@ docker run -d --net=host --rm --name es_main_01 \
         -v /data/eldata/data:/usr/share/elasticsearch/data \
         -v /data/eldata/logs:/usr/share/elasticsearch/logs \
         -e cluster.name=es_main_v2 \
-        -e node.name=es_main_01_v2 \
+        -e node.name=es_main_01 \
         -e node.master=true  \
         -e node.data=true  \
         -e network.host=0.0.0.0  \
         -e discovery.seed_hosts=v2-main-scan02,v2-main-scan03 \
-        -e cluster.initial_master_nodes=es_main_01_v2 \
+        -e cluster.initial_master_nodes=es_main_01 \
         -e xpack.security.enabled=false \
         -e bootstrap.memory_lock=true --ulimit memlock=-1:-1 \
         -e "ES_JAVA_OPTS=-Xms24g -Xmx24g" \
