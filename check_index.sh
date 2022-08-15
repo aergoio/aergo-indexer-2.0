e419958cd5fb1a57b86f418d99962742d66908f2
Kjun Seo <kjseo@blocko.io>
2022-08-15 09:03:34 +0900

    refact shell

--- check_index.sh
+++ check_index.sh
@@ -1,5 +1,5 @@
 OS=ubuntu:22.04
 docker rm -f check_idx
-docker run --rm -it --name check_idx --net=host --privileged \
+docker run --rm -d -it --name check_idx --net=host --privileged \
 	-v $(pwd):/home \
 	$OS bash /home/bin/check_$1.sh
--- sync_index.sh
+++ sync_index.sh
@@ -1,5 +1,5 @@
 OS=ubuntu:22.04
 docker rm -f sync_idx
-docker run --rm -it --name sync_idx --net=host --privileged \
+docker run -d --rm -it --name sync_idx --net=host --privileged \
 	-v $(pwd):/home \
 	$OS bash /home/bin/sync_$1.sh
