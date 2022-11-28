ec94351eed7e14bcb984f87497b55043f95e3324
김창학 <sjc02183@naver.com>
2022-11-28 12:03:51 +0900

    add unit test - category.go

--- go.mod
+++ go.mod
@@ -10,11 +10,13 @@ require (
 	github.com/mr-tron/base58 v1.2.0
 	github.com/olivere/elastic/v7 v7.0.31
 	github.com/spf13/cobra v1.3.0
+	github.com/stretchr/testify v1.8.1
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.1-0.20180906183839-65a6292f0157 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -25,13 +27,13 @@ require (
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/zerolog v1.16.1-0.20191111091419-e709c5d91e35 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.10.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
@@ -39,4 +41,5 @@ require (
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ indexer/category/category_test.go
@@ -0,0 +1,189 @@
+package category
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/aergoio/aergo-indexer-2.0/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDetectTxCategory(t *testing.T) {
+	fn_diff := func(Tx *types.Tx, categoryExpect TxCategory, callNameExpect string) {
+		category, callName := DetectTxCategory(Tx)
+		require.Equal(t, categoryExpect, category)
+		require.Equal(t, callNameExpect, callName)
+	}
+
+	// Redeploy
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_REDEPLOY}}, Redeploy, "")
+
+	// MultiCall
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_MULTICALL}}, MultiCall, "")
+
+	// Deploy
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_NORMAL,
+			Recipient: nil,
+			Payload:   []byte{1, 2, 3, 4}, // deploy contract
+		},
+	}, Deploy, "")
+
+	// Cluster
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.enterprise"),
+			Payload: []byte(`{
+				"Name": "changeCluster",
+				"Args": [
+				  {
+					"command": "remove",
+					"id": "ee72676e83929233"
+				  }
+				]
+			}`),
+		},
+	}, Cluster, "changecluster")
+
+	// Conf
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.enterprise"),
+			Payload: []byte(`{
+				"Name": "changeConf",
+				"Args": [
+				  {
+					"command": "remove",
+					"id": "ee72676e83929233"
+				  }
+				]
+			}`),
+		},
+	}, Conf, "changeconf")
+
+	// Enterprise
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.enterprise"),
+			Payload: []byte(`{
+				"Name": "appendAdmin",
+				"Args": [
+				  "AmLjqiQiMiG44oc9aucS9XTxEdHfcMesLVRWS5VZtuK9Vo4ddecF"
+				]
+			}`),
+		},
+	}, Enterprise, "appendadmin")
+
+	// NameCreate
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.name"),
+			Payload: []byte(`{
+				"Name": "v1createName",
+				"Args": [
+				"abcdefg12345"
+				]
+			}`),
+		},
+	}, NameCreate, "v1createname")
+
+	// NameUpdate
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.name"),
+			Payload: []byte(`{
+				"Name": "v1updateName",
+				"Args": [
+				  "tokenlockerr",
+				  "AmhXTtDUv7ZCJHB8Bz29S5F8pEj5F6gmb6wGLzUn7ADwergXNJAc"
+				]
+			}`),
+		},
+	}, NameUpdate, "v1updatename")
+
+	// Name
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.name"),
+			Payload: []byte(`{
+				"Name": "v1setOwner",
+				"Args": [
+				  "AmhJX1FYQKqNuhwBACW7fkxcTHdMakAfMQobuD5QNXJwT1ZgriAc"
+				]
+			}`),
+		},
+	}, Name, "v1setowner")
+
+	// Staking
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.system"),
+			Payload: []byte(`{
+				"Name": "v1stake"
+			}`),
+		},
+	}, Staking, "v1stake")
+
+	// Voting
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_GOVERNANCE,
+			Recipient: []byte("aergo.system"),
+			Payload: []byte(`{
+				"Name": "v1voteBP",
+				"Args": [
+				  "16Uiu2HAmGiJ2QgVAWHMUtzLKKNM5eFUJ3Ds3FN7nYJq1mHN5ZPj9"
+				]
+			}`),
+		},
+	}, Voting, "v1votebp")
+
+	// System ( not exist now )
+
+	// Governance
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_GOVERNANCE}}, Governance, "")
+
+	// Call
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_CALL,
+			Recipient: []byte{12, 171, 69, 173, 86, 155, 29, 39, 247, 12, 159, 32, 253, 97, 50, 76, 129, 108, 21, 82, 227, 57, 171, 87, 153, 60, 50, 199, 126, 40, 150, 147, 124},
+			Payload: []byte(`{
+				"Name": "invoke",
+				"Args": [
+				"claimBylTokenReward",
+				"AmPDbCi6D5EatLGco42x4zyqFV88f4EiV8Ge3TvEAN7yHYEUBeCW",
+				"5859000000000000"
+				]
+			}`),
+		},
+	}, Call, "invoke")
+
+	// Payload
+	fn_diff(&types.Tx{
+		Body: &types.TxBody{
+			Type:      types.TxType_TRANSFER,
+			Recipient: []byte{12, 171, 69, 173, 86, 155, 29, 39, 247, 12, 159, 32, 253, 97, 50, 76, 129, 108, 21, 82, 227, 57, 171, 87, 153, 60, 50, 199, 126, 40, 150, 147, 124},
+			Payload: []byte(`{
+				"chainID": "CHHjUaP3Euuxb4hC63j6otitzLfieFimqBxdhMt3NSE2",
+				"bestBlockHeight": 27421809,
+				"bestBlockHash": "6k8WnEKMjm7ofBP3wtQFZn3DxAD97FzxZpayAHPX9kyq"
+			}`),
+		},
+	}, Payload, "")
+
+	// Transfer
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_TRANSFER, Amount: big.NewInt(100).Bytes()}}, Transfer, "")
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_NORMAL, Amount: big.NewInt(100).Bytes()}}, Transfer, "")
+
+	// None
+	fn_diff(&types.Tx{Body: &types.TxBody{Type: types.TxType_NORMAL}}, None, "")
+}
