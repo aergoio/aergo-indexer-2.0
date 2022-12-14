0e4f7fc80d8fb39c746ff76e4454132190b44e1a
william <cwlim0305@gmail.com>
2022-12-14 11:34:55 +0900

    test code

---
+++ es_test_cw.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+VERSION=${VERSION:=7.15.2}
+ELASTIC=${ELASTIC:=docker.elastic.co/elasticsearch/elasticsearch:$VERSION}
+
+echo $ELASTIC
+docker rm -f es_node
+echo "Starting elasticsearch"
+docker pull $ELASTIC
+docker run -d --rm -p 9200:9200 -p 9300:9300 --name es_node \
+        -v /home/seo/data/eldata/data:/usr/share/elasticsearch/data \
+        -v /home/seo/data/eldata/logs:/usr/share/elasticsearch/logs \
+        -e "discovery.type=single-node"  \
+        -e "xpack.security.enabled=false" \
+        -e "bootstrap.memory_lock=true" --ulimit memlock=-1:-1 \
+        -e "ES_JAVA_OPTS=-Xms24g -Xmx24g" \
+        $ELASTIC
+
