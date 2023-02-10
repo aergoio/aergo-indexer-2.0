3e339dfd55e2933f73607c1b6e02a215b6730277
김창학 <sjc02183@naver.com>
2023-02-10 14:47:36 +0900

    add test

--- indexer/documents/conv_test.go
+++ indexer/documents/conv_test.go
@@ -13,6 +13,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestAccountEncodeDecode(t *testing.T) {
+	fn_test := func(account string) {
+		enc := DecodeAccount(account)
+		dec := EncodeAccount(enc)
+		require.Equal(t, account, dec)
+	}
+
+	fn_test("aergo.system")
+	fn_test("aergo.vault")
+	fn_test("aergossystem")
+	fn_test("AmPxDZYmc7f6cTEzv8rTkbDxYtziTkRQEBbCW9r5GgVntpSmgXWb")
+	fn_test("AmMK3LZiR1oEf66xzXir7mA5SUVVHSinWUYmh5FwueoVmciH3CuJ")
+	fn_test("AmMjrVRQbrgDYChnWgyYL6gfneGT5ui6DwuvUXp8nTdUz8wwstAq")
+	fn_test("AmPERyyJgoDLm6GBTEaVSwennQeyQGDSFycGUuSMsvupL1qKfTFo")
+}
+
 func TestConvBlock(t *testing.T) {
 	fn_test := func(aergoBlock *types.Block, blockProducer string, esBlockExpect EsBlock) {
 		esBlockConv := ConvBlock(aergoBlock, blockProducer)
